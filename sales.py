ae153877732569b014bdf0ce8271914ed50da7b8
C3NZ <cenz@cenz.io>
2019-04-26 17:08:54 -0700

    [feat] create a correlation plot to show the relationship between the features of our dataset

--- sales.py
+++ sales.py
@@ -1,6 +1,26 @@
 import argparse
 
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+def create_model():
+    pass
+
+
+def visualize_correlation(sales_data: pd.DataFrame):
+    """
+        Visualize the correlation between all variables
+    """
+    correlation = sales_data.corr()
+    sns.heatmap(
+        correlation, xticklabels=correlation.columns, yticklabels=correlation.columns
+    )
+    plt.show()
 
 
 def main():
@@ -11,7 +31,8 @@ def main():
     channels = sales_data["Channel"]
 
     sales_data.drop(labels=["Channel", "Region"], axis=1, inplace=True)
-    print(sales_data)
+    print(sales_data.to_numpy())
+    visualize_correlation(sales_data)
 
 
 if __name__ == "__main__":
